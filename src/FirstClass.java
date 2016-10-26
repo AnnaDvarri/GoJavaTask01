7a067e613b0c72c088dea4b9ca8f4fd310c0e07e
Loki_ <annadvarri@gmail.com>
2016-10-26 15:13:16 +0300

    first task

---
+++ src/FirstClass.java
@@ -0,0 +1,9 @@
+/**
+ * Created by User on 26-Oct-16.
+ */
+public class FirstClass {
+    public static void main(String[] args) {
+        String str = "Мой первый класс на Java";
+        System.out.println(str);
+    }
+}
